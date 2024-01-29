08f37a95c946a9b300d86fe832442003059a56e6
dieromeo <diegoromeo99@icloud.com>
2024-01-29 23:25:06 +0100

    feat: model e controller

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,12 +5,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @vite(['resources/scss/app.scss', 'resources/js/app.js'])
-    <title>Template title</title>
+    <title>Film list</title>
 </head>
 
 <body>
-    <h1>title</h1>
-    <!-- <img src="{{ Vite::asset('resources/img/laravel-logo.webp') }}" alt=""> -->
 </body>
 
 </html>
