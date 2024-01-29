79b22bfab38310dc3f8300daa719ab476c2d1530
dieromeo <diegoromeo99@icloud.com>
2024-01-29 23:48:05 +0100

    feat: film cards

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,32 @@
     <title>Film list</title>
 </head>
 
-<body>
+<body class="bg-dark">
+    <header>
+        <h1 class="text-center text-primary text-uppercase py-4">Lista film</h1>
+    </header>
+    <main>
+        <div class="container py-5">
+            <div class="row g-5">
+                @foreach ($films as $film)
+                    <div class="col-4 col-lg-3">
+                        <div class="card bg-info-subtle" style="width: 18rem; height: 15rem;">
+                            <div class="card-header text-uppercase text-center p-3">
+                                {{ $film['title'] }}
+                            </div>
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item bg-info-subtle">Data di uscita: {{ $film['date'] }}</li>
+                                <li class="list-group-item bg-info-subtle">Titolo originale:
+                                    {{ $film['original_title'] }}</li>
+                                <li class="list-group-item text-center text-success bg-info-subtle">
+                                    Voto: {{ $film['vote'] }}</li>
+                            </ul>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    </main>
 </body>
 
 </html>
